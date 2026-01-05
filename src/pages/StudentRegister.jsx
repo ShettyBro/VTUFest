da9b9cd1028e0b31f0352e29bb0409ecbd0e55a7
Rohith-2504 <brohithreddy9731@gmail.com>
2026-01-05 15:11:14 +0530

    0.2

--- src/pages/StudentRegister.jsx
+++ src/pages/StudentRegister.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/studentRegister.css";
+import "../styles/StudentRegister.css";
 
 /* ================= COLLEGE MASTER ================= */
 export const COLLEGES = [
