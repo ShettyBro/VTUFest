c4ddd2960f4cf618bc83baa69c7845fbd2dadf37
Rohith-2504 <brohithreddy9731@gmail.com>
2026-01-05 14:57:23 +0530

    1.3

--- src/pages/PrincipalDashboard.jsx
+++ src/pages/PrincipalDashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Layout from "../components/layout/layout";
-import "../styles/PrincipalDashboard.css";
+import "../styles/principalDashboard.css";
 import CampusMap from "../components/CampusMap";
 
 export default function PrincipalDashboard() {
