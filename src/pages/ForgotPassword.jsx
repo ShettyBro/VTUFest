f573c5d8ceb54e7cd89d0ac9874cae0b3a18b0cc
Sudeep  Shivashettar <109416576+ShettyBro@users.noreply.github.com>
2026-01-05 14:26:26 +0530

    1.1

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/ForgotPassword.css";
+import "../styles/forgotPassword.css";
 
 export default function ForgotPassword() {
   const navigate = useNavigate();
