8d346bd07239436edb2e6e1635721182fa64a981
Sudeep  Shivashettar <109416576+ShettyBro@users.noreply.github.com>
2026-01-13 23:23:45 +0530

    test 5

--- src/pages/PrincipalDashboard.jsx
+++ src/pages/PrincipalDashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Layout from "../components/layout/layout";
 import ManagerProfileModal from "./ManagerProfileModal";
-import FinalApprovalOverlay from "./ApprovalOverlayy";
+import FinalApprovalOverlay from "./ApprovalOverlay";
 import CampusMap from "../components/CampusMap";
 import "../styles/PrincipalDashboard.css";
 
