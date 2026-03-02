34186e007c60c1ef9ad3985492eb3fdf3c8a37b5
Sudeep  Shivashettar <109416576+ShettyBro@users.noreply.github.com>
2026-03-02 13:37:14 +0530

    0.083

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -453,7 +453,7 @@ export default function Dashboard() {
 
             {/* --- LEFT COL: CALENDAR --- */}
             <div className="glass-card calendar-card">
-              <h3>Upcoming Events</h3>
+              <h3>Calendar</h3>
               <div className="calendar-mobile-scroll calendar-list">
                 {eventsCalendarData.calendarEvents
                   .slice()
