2ee598eaf560472e2e63bb3bc265213cf7d68046
Sudeep  Shivashettar <109416576+ShettyBro@users.noreply.github.com>
2026-03-26 12:00:14 +0530

    locks fixed

--- src/pages/ManagerDashboard.jsx
+++ src/pages/ManagerDashboard.jsx
@@ -215,7 +215,7 @@ export default function ManagerDashboard() {
 
       if (data.success) {
         setLockStatus(data);
-        if (data.is_locked) {
+        if (data.is_locked || data.manager_lock) {
           setShowFinalApprovalOverlay(true);
         }
       }
